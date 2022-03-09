ee1a9cb393d674f8d313ac7b98879e059b2aabdd
ORLANDO DOS SANTOS DOURADO <62121619+OrlandoSD@users.noreply.github.com>
2022-03-09 11:59:11 -0300

    Add files via upload

---
+++ lab-padrao-projeto/src/main/java/one/digitalinnovation/gof/Application.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring Boot gerado via Spring initialazr
+ * Os seguintes modulos foram selecionados
+ * spring web
+ * H2 Database
+ * OpenFeign
+ *
+ * @author OrlandoSD
+ */
+@EnableFeignClients
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
